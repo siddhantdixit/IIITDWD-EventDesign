e9da2cd1c5cac4477c01dcc5e63337795f9bb803
Sreedeva_Reddy <sreedevareddy.27@gmail.com>
2021-08-01 13:56:38 +0530

    timer mobile

--- js/main.js
+++ js/main.js
@@ -134,7 +134,7 @@ jQuery(document).ready(function( $ ) {
   });
 
   // Timer
-var target_date = Date.parse("Jan 16, 2022")
+var target_date = Date.parse("Aug 15, 2021")
 var days, hours, minutes, seconds; // variables for time units
 
 var countdown = document.getElementById("tiles"); // get tag element
